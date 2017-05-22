fc2d84937294a8188b215912c8fdca41f3bbd09e
pjkohli <pjkohli17@gmail.com>
2017-05-22 08:35:24 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,20 +10,22 @@ public class Main {
 
         List<Product> list = new ArrayList<>();
 
+        //Type1 - buy 3 (equals) items and pay for 2
         //Promotion Text - "Buy 3 for 2"
         Product product1 = new Product(1,"Pringles 200g", (float) 1.99,"Type1");
         list.add(product1);
 
+        //Type2 - buy 2 (equals) items for a special price
         //Promotion Text - "Buy 2 for 1.50"
         Product product2 = new Product(2,"Banana chips 500g",(float) 1.00,"Type2");
         product2.setSpecialPrice((float) 1.50);
         list.add(product2);
 
-        //Promotion Text - "Free Product" , Buy 5 get 2 500 ml free
+        //Type4 - Free Product for Type3
         Product product3 = new Product(3,"Coca Cola 500ml",(float) 1.00,"Type5");
-        //product3.setQuantitiesFree(2);
         list.add(product3);
 
+        //Type3 - for each N (equals) items X, you get K items Y for free
         //Promotion Text - "Buy 5 get 2 500 ml free"
         Product product4 = new Product(4,"Coca Cola 2L",(float) 2.00,"Type3");
         product4.setQuantitiesToBuy(5);
@@ -31,18 +33,22 @@ public class Main {
         product4.setFreeProduct(product3);
         list.add(product4);
 
+        //Type4 - buy 3 (in a set of items) and the cheapest is free
         //Promotion Text - "Buy any 3 for 2"
         Product product5 = new Product(5,"Coriander 80g",(float) 1.00,"Type4");
         list.add(product5);
 
+        //Type4 - buy 3 (in a set of items) and the cheapest is free
         //Promotion Text - "Buy any 3 for 2"
         Product product6 = new Product(6,"Parsley 80g",(float) 1.50,"Type4");
         list.add(product6);
 
+        //Type4 - buy 3 (in a set of items) and the cheapest is free
         //Promotion Text - "Buy any 3 for 2"
         Product product7 = new Product(7,"Basil 80g",(float) 2.50,"Type4");
         list.add(product7);
 
+        //Type6 - If no promotion text
         //Promotion Text - "No promotion"
         Product product8 = new Product(8,"Butter",(float) 2.00,"Type6");
         list.add(product8);
@@ -71,6 +77,8 @@ public class Main {
                 if (list.get(j).getProductId() == id[i]) {
                     price = list.get(j).getPrice();
                     System.out.println("The original price of "+id[i]+"-" + list.get(j).getProductName() + " is: " + price);
+                    //Calculate number of free products
+                    //Add Cost of Type3 products to grand total
                     if (list.get(j).getPromotionType() == "Type3") {
                         toBuy = list.get(j).getQuantitiesToBuy();
                         grandTotalPrice += price * quantity[i];
@@ -85,24 +93,33 @@ public class Main {
             for(int j=0;j< list.size();j++) {
                 if(list.get(j).getProductId() == id[i]){
                     price = list.get(j).getPrice();
+                    //Add Cost of Type1 products to grand total
                     if(list.get(j).getPromotionType() == "Type1"){
                         divisor = quantity[i]/3;
                         remainder = quantity[i]%3;
                         grandTotalPrice += price*((divisor*2)+remainder);
-                    } else if(list.get(j).getPromotionType() == "Type2"){
+                    }
+                    //Add Cost of Type2 products to grand total
+                    else if(list.get(j).getPromotionType() == "Type2"){
                         divisor = quantity[i]/2;
                         remainder = quantity[i]%2;
                         float specialPrice = list.get(j).getSpecialPrice();
                         grandTotalPrice += (divisor*specialPrice)+(remainder*price);
-                    } else if(list.get(j).getPromotionType() == "Type4"){
+                    }
+                    //Create array of prices of all products in Type4
+                    else if(list.get(j).getPromotionType() == "Type4"){
                         counterForType4 +=quantity[i];
                         priceList[k]=price;
                         quantityList[k]=quantity[i];
                         k++;
-                    } else if(list.get(j).getPromotionType() == "Type5"){
+                    }
+                    //Add Cost of Type5 products to grand total
+                    else if(list.get(j).getPromotionType() == "Type5"){
+                        //No free product, add all products cost to Total
                         if(z==0){
                             grandTotalPrice += quantity[i] * price;
                         }
+                        //Add Cost by subtracting free products
                         else {
                             for (int l = 0; l < z; l++) {
                                 if (freeId[l] == id[i]) {
@@ -112,7 +129,9 @@ public class Main {
                                 }
                             }
                         }
-                    } else if(list.get(j).getPromotionType() == "Type6"){
+                    }
+                    //Add Cost of Type6 products to grand total
+                    else if(list.get(j).getPromotionType() == "Type6"){
                         grandTotalPrice += price*quantity[i];
                     }
                 }
@@ -122,9 +141,13 @@ public class Main {
         int loc = -1;
         int counter = 0;
         int totalCounter;
+        //For Type4 products,
+        //First calculate number of products payable in totalCounter
         if (!(counterForType4<3)){
             totalCounter = ((counterForType4/3)*2)+(counterForType4%3);
         }else totalCounter = counterForType4;
+
+        //Add all the highest paid products, where number of products = totalCounter
         while(counter<totalCounter){
             for(i=0;i<k;i++){
                 if(big<priceList[i]){
